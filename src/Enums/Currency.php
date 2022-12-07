2fefbedd1a3387bcf0e1973c369953d8caa793ef
Aleksandr Sadovoj <sadovojav@gmail.com>
2022-12-07 16:20:10 +0200

    Update supported currency

--- src/Enums/Currency.php
+++ src/Enums/Currency.php
@@ -6,5 +6,47 @@ namespace Bitcart\Enums;
 
 enum Currency: string
 {
+    case ARS = 'ARS';
+    case AUD = 'AUD';
+    case BHD = 'BHD';
+    case BMD = 'BMD';
+    case BRL = 'BRL';
+    case CAD = 'CAD';
+    case CHF = 'CHF';
+    case CLP = 'CLP';
+    case CNY = 'CNY';
+    case CZK = 'CZK';
+    case DKK = 'DKK';
+    case EUR = 'EUR';
+    case GBP = 'GBP';
+    case HKD = 'HKD';
+    case HUF = 'HUF';
+    case IDR = 'IDR';
+    case ILS = 'ILS';
+    case INR = 'INR';
+    case JPY = 'JPY';
+    case KRW = 'KRW';
+    case KWD = 'KWD';
+    case LKR = 'LKR';
+    case MMK = 'MMK';
+    case MXN = 'MXN';
+    case MYR = 'MYR';
+    case NGN = 'NGN';
+    case NOK = 'NOK';
+    case NZD = 'NZD';
+    case PHP = 'PHP';
+    case PKR = 'PKR';
+    case PLN = 'PLN';
+    case RUB = 'RUB';
+    case SAR = 'SAR';
+    case SEK = 'SEK';
+    case SGD = 'SGD';
+    case THB = 'THB';
+    case TRY = 'TRY';
+    case TWD = 'TWD';
+    case UAH = 'UAH';
     case USD = 'USD';
+    case VEF = 'VEF';
+    case VND = 'VND';
+    case ZAR = 'ZAR';
 }
