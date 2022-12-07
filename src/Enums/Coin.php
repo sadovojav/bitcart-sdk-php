3cbc16ab15dd0531fc842412fa57459315eb9d2c
Aleksandr Sadovoj <sadovojav@gmail.com>
2022-12-07 16:20:29 +0200

    Update supported coins

--- src/Enums/Coin.php
+++ src/Enums/Coin.php
@@ -7,5 +7,14 @@ namespace Bitcart\Enums;
 enum Coin: string
 {
     case BTC = 'btc';
+    case BTH = 'bth';
+    case ETH = 'eth';
+    case BNB = 'bnb';
+    case SBSH = 'sbsh';
+    case LTC = 'ltc';
+    case MATIC = 'matic';
+    case BSTY = 'bsty';
     case TRX = 'trx';
+    case XRG = 'xrg';
+    case GRS = 'grs';
 }
